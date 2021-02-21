47ae9664f1cfb83a6e75fa648ee59a256ffc3455
awefw7 <72689987+awefw7@users.noreply.github.com>
2021-02-21 10:59:33 -0800

    Add files via upload

---
+++ FavouriteSong.swift
@@ -0,0 +1,66 @@
+
+
+
+/*Creating the attributes of the favourite song,
+with adding the names of the attributes,
+and printing it to the screen.
+You see the multi-line comment*/
+
+let title : String = "Favourite song"
+print(title)
+let attribute1 : String = "Artist:" //These are the attributes. You see single-line comment
+var artist : String = "Alesha Dixon" //These are the values of the attributes
+let attribute2 : String = "Genre:"
+var genre1 : String = "R&B- "
+var genre2 : String = "Dance-Pop- "
+var genre3 : String = "mambo- "
+var genre4 : String = "swing- "
+var genre5 : String = "jazz"
+let attribute3 : String = "SongLength:"
+var songLength : Int = 214
+let attribute4 : String = "Label:"
+var label : String = "Asylum"
+let attribute5 : String = "SongWriters:"
+var songWriter1 : String = "Alesha Dixon, "
+var songWriter2 : String = "Brian Higgins, "
+var songWriter3 : String = "Miranda Cooper, and "
+var songWriter4 : String = "Carla Marie Williams"
+let attribute6 : String = "Producer:"
+var producer : String = "Xenomania"
+let attribute7 : String = "StartingYear:"
+var startingYear : Int = 2008
+let attribute8 : String = "SoldinAustralia"
+var soldinAustralia : Int = 35000
+let attribute9 : String = "SoldinFinland:"
+var soldinFinland : Int = 5983
+let attribute10 : String = "SoldinSpain:"
+var soldinSpain : Int = 80000
+let attribute11 : String = "SoldinUK:"
+var soldinUK : Int = 400000
+let attribute12 : String = "MeanSold:"
+var meanSold : Double = Double (soldinAustralia + soldinFinland + soldinSpain + soldinUK) / 4 //This is the math calculation
+
+print(attribute1) //These are the printing of the attributes and their values to the screen
+print(artist)
+print(attribute2)
+print(genre1+genre2+genre3+genre4+genre5)
+print(attribute3)
+print(songLength)
+print(attribute4)
+print(label)
+print(attribute5)
+print(songWriter1+songWriter2+songWriter3+songWriter4)
+print(attribute6)
+print(producer)
+print(attribute7)
+print(startingYear)
+print(attribute8)
+print(soldinAustralia)
+print(attribute9)
+print(soldinFinland)
+print(attribute10)
+print(soldinSpain)
+print(attribute11)
+print(soldinUK)
+print(attribute12)
+print(meanSold) //This is the Decimal(floats)
